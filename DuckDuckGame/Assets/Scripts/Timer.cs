8bf3a5159b53fddcf102d5070a413d47aaa81ee2
hktinder <htinder@email.sc.edu>
2025-07-23 15:53:01 -0400

    Updated timer and win logic

--- DuckDuckGame/Assets/Scripts/Timer.cs
+++ DuckDuckGame/Assets/Scripts/Timer.cs
@@ -1,5 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
-using TMPro; 
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
@@ -13,13 +16,15 @@ public class Timer : MonoBehaviour
 
         if (remainingTime > 0)
         {
-            remainingTime -= Time.deltaTime;        
+            remainingTime -= Time.deltaTime;
         }
         else if (remainingTime < 0)
         {
             remainingTime = 0;
-            timerText.color = Color.red; 
+            timerText.color = Color.red;
             // check if cake count goal was met to go to win or lose screen 
+            // temp code added for proof of concept
+            SceneManager.LoadScene("WinScreen");
         }
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
